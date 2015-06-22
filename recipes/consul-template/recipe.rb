d238ba9eff81d905855c459d8b8ea4d2853f1413
Pawel Chojnacki <pawel@chojnacki.ws>
2015-06-22 17:41:07 +0200

    consul template v0.10-wikia1

--- recipes/consul-template/recipe.rb
+++ recipes/consul-template/recipe.rb
@@ -1,11 +1,11 @@
 class ConsulTemplate < FPM::Cookery::Recipe
-    source 'http://github.com/hashicorp/consul-template/releases/download/v0.9.0/consul-template_0.9.0_linux_amd64.tar.gz' 
-    sha256 '3d8c9fcaee18a4369cc731528ce9d6a5be03d88b954a2fea0f4406fc54c70fc8'
+    source 'https://github.com/hashicorp/consul-template/releases/download/v0.10.0/consul-template_0.10.0_linux_amd64.tar.gz' 
+    sha256 'ef298a2ae54cf51dbfc4108194299a9055b252ff9b917e7dd40c72fa30820096'
 
     name 'consul-template'
     
-    version  '0.9.0'
-    revision '1'
+    version  '0.10.0'
+    revision 'wikia1'
     def install
     	bin.install ['consul-template']
     end
