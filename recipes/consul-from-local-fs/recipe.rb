18a291cf96dff3249c61ab87cd2a64893b771615
Pawel Chojnacki <pawel@chojnacki.ws>
2015-07-15 16:20:04 +0200

    ad pre and post uninstall scripts

--- recipes/consul-from-local-fs/recipe.rb
+++ recipes/consul-from-local-fs/recipe.rb
@@ -1,13 +1,24 @@
 class Consul < FPM::Cookery::Recipe
-    source 'file:///vagrant/recipes/consul-from-local-fs/consul' 
-    name 'consul'
-    
-    version  '0.5.2'
-    revision 'wikia2'
-    def install
-    	bin.install ['consul']
-    end
+  source 'file:///vagrant/recipes/consul-from-local-fs/consul' 
+  name 'consul'
+  description 'Consul with TCP fallback backport, package built using https://github.com/pchojnacki/fpm-recipes'
+  homepage 'https://github.com/pchojnacki/fpm-recipes'
+  
+  version  '0.5.2'
+  revision 'wikia3'
 
-    def build
-    end
+  post_install 'post-install'
+  pre_install 'pre-install'
+  pre_uninstall 'pre-uninstall'
+
+
+  def install
+    bin.install ['consul']
+    etc('consul.d').mkdir
+    etc('init').install_p workdir('consul.conf')
+    var('lib/consul').mkdir
+  end
+
+  def build
+  end
 end
