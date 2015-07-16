0211994cc18b8e6c4cb60c6531722c018c26c61d
Pawel Chojnacki <pawel@chojnacki.ws>
2015-07-16 14:44:43 +0200

    build consul from sources

--- recipes/consul-from-local-fs/recipe.rb
+++ recipes/consul-from-local-fs/recipe.rb
@@ -1,24 +1,26 @@
 class Consul < FPM::Cookery::Recipe
-  source 'file:///vagrant/recipes/consul-from-local-fs/consul' 
+  source 'file:///vagrant/recipes/consul-from-local-fs/consul.conf'
   name 'consul'
   description 'Consul with TCP fallback backport, package built using https://github.com/pchojnacki/fpm-recipes'
   homepage 'https://github.com/pchojnacki/fpm-recipes'
   
   version  '0.5.2'
-  revision 'wikia4'
+  revision 'wikia5'
 
   post_install 'post-install'
   pre_install 'pre-install'
   pre_uninstall 'pre-uninstall'
 
-
   def install
-    bin.install ['consul']
+    bin.install workdir('gopath/bin/consul')
     etc('consul.d').mkdir
     etc('init').install_p workdir('consul.conf')
     var('lib/consul').mkdir
   end
 
   def build
+    ENV['GOPATH'] = workdir("gopath")
+    safesystem('cd /vagrant; git submodule update --init --recursive')
+    safesystem('cd $GOPATH/src/github.com/hashicorp/consul; make')
   end
 end
