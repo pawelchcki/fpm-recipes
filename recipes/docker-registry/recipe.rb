5e401cfe15b896b4fa5007e3482ac98c4516693e
Pawel Chojnacki <pawel@chojnacki.ws>
2015-10-06 15:34:38 +0200

    Docker registry use version with radosgw support

--- recipes/docker-registry/recipe.rb
+++ recipes/docker-registry/recipe.rb
@@ -5,7 +5,7 @@ class DockerRegistry < FPM::Cookery::Recipe
   homepage 'https://github.com/pchojnacki/fpm-recipes'
   
   version  '2.1.1'
-  revision 'wikia1'
+  revision 'wikia2'
 
   # post_install 'post-install'
   # pre_install 'pre-install'
@@ -25,8 +25,8 @@ class DockerRegistry < FPM::Cookery::Recipe
     distDir = ENV['GOPATH'] + "/src/github.com/docker/distribution"
 
     safesystem('go get github.com/tools/godep')
-    safesystem("git clone https://github.com/docker/distribution.git #{distDir}")
-    safesystem("cd #{distDir}; git reset --hard v#{version}")
+    safesystem("git clone https://github.com/lorieri/distribution.git #{distDir}")
+    safesystem("cd #{distDir}; git reset --hard radosgw")
     safesystem("cd #{distDir}; GOPATH=`$GOPATH/bin/godep path`:$GOPATH make PREFIX=#{workdir("tmp-build")} binaries")
   end
 end
