aa4ccf1dcd578197f1b20b51336b4987170c34bc
Pawel Chojnacki <pawel@chojnacki.ws>
2016-01-13 09:36:16 +0100

    consul 0.6.1 new consul-web-ui

--- recipes/consul/recipe.rb
+++ recipes/consul/recipe.rb
@@ -1,10 +1,10 @@
 class Consul < FPM::Cookery::Recipe
-    source 'https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip' 
-    sha256 '171cf4074bfca3b1e46112105738985783f19c47f4408377241b868affa9d445'
+    source 'https://releases.hashicorp.com/consul/0.6.1/consul_0.6.1_linux_amd64.zip' 
+    sha256 'dbb3c348fdb7cdfc03e5617956b243c594a399733afee323e69ef664cdadb1ac'
 
     name 'consul'
     
-    version  '0.5.2'
+    version  '0.6.1'
     revision '1'
     def install
     	bin.install ['consul']
