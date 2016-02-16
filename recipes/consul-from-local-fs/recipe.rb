b2e387bb4548cb7faa3cebd8d65d1de6c074e2dc
Pawel Chojnacki <pawel@chojnacki.ws>
2016-02-16 16:43:56 +0100

    0.6.3 consul recipe

--- recipes/consul-from-local-fs/recipe.rb
+++ recipes/consul-from-local-fs/recipe.rb
@@ -1,11 +1,11 @@
 class Consul < FPM::Cookery::Recipe
-  source 'file:///vagrant/recipes/consul-from-local-fs/consul.conf'
+  source 'file:///vagrant/recipes/consul-from-local-fs/consul'
   name 'consul'
   description 'Consul with TCP fallback backport, package built using https://github.com/pchojnacki/fpm-recipes'
   homepage 'https://github.com/pchojnacki/fpm-recipes'
   
-  version  '0.5.2'
-  revision 'wikia11'
+  version  '0.6.3'
+  revision 'wikia1'
 
   post_install 'post-install'
   pre_install 'pre-install'
@@ -20,10 +20,19 @@ class Consul < FPM::Cookery::Recipe
 
   def build
     ENV['GOPATH'] = workdir("gopath")
-    safesystem('cd /vagrant; git submodule update --init --recursive')
-    safesystem("cd /vagrant; git submodule foreach git reset --hard")
-    safesystem("cd $GOPATH/src/github.com/hashicorp/consul; git apply #{workdir("0001-Change-configs-to-reduce-the-chance-of-node-being-ma.patch")}")
-    safesystem('cd $GOPATH/src/github.com/hashicorp/consul; make')
-    safesystem("cd /vagrant; git submodule foreach git reset --hard") # cleanup after self
+    # workdir('/vagrant')
+    # mkdir_p('build')
+    # workdir('/vagrant/build')
+    safesystem("chmod +x /usr/local/bin/godep")
+    safesystem('curl -sSL https://github.com/hashicorp/consul/archive/v0.6.3.tar.gz | tar --strip-components 1 -xzf  -')
+    safesystem("mkdir Godeps && cp deps/v0-6-3.json Godeps/Godeps.json")
+    safesystem("mkdir #{workdir("gopath")}/src/github.com/hashicorp; ln -s #{builddir} #{workdir("gopath")}/src/github.com/hashicorp/consul || true")
+    safesystem("godep restore")
+    safesystem("cd #{workdir("gopath")}/src/github.com/hashicorp/consul && make")
+    # safesystem('cd /vagrant; git submodule update --init --recursive')
+    # safesystem("cd /vagrant; git submodule foreach git reset --hard")
+    # safesystem("cd $GOPATH/src/github.com/hashicorp/consul; git apply #{workdir("0001-Change-configs-to-reduce-the-chance-of-node-being-ma.patch")}")
+    # safesystem('cd $GOPATH/src/github.com/hashicorp/consul; make')
+    # safesystem("cd /vagrant; git submodule foreach git reset --hard") # cleanup after self
   end
 end
